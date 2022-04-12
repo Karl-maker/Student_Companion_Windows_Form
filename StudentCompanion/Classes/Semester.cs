cb00f456f2e405a962991e27ccd6fbd8f83b8690
karl-maker <karljohanbailey98@gmail.com>
2022-04-12 12:25:02 -0400

    Semester Class Connection to DB

--- StudentCompanion/Classes/Semester.cs
+++ StudentCompanion/Classes/Semester.cs
@@ -12,6 +12,7 @@ namespace StudentCompanion
         private int _id;
         private float _gpa;
         private int _credits;
+        private int _student_id;
         private DateTime _start_date = new DateTime();
         private DateTime _end_date = new DateTime();
 
@@ -20,62 +21,147 @@ namespace StudentCompanion
             // This will be used for creating a new Semester
         }
 
-        Semester(int id, DateTime start_date, DateTime end_date )
+        Semester(int id, int student_id, DateTime start_date, DateTime end_date )
         {
             // Load semester id from Database and update all attributes
             _id = id;
             _start_date = start_date;
             _end_date = end_date;
+            _student_id = student_id;
         }
 
-        public int id => _id;
+        public int id => this._id;
 
-        public float gpa => _gpa; 
+        public int student_id
+        {
+            get
+            {
+                return this._student_id;
+            }
+            set
+            {
+                // Check if DateTime
+                this._student_id = value;
+            }
+        }
+
+        public float gpa => this._gpa; 
 
         public int credits
         {
             get
             {
                 // Get total credits database from courses
-                return _credits;
+                return this._credits;
             }
         
         }
 
         public DateTime start_date
         {
-            get { return _start_date; }
+            get { return this._start_date; }
             set {  
                 // Check if DateTime
-                _start_date = value; 
+                this._start_date = value; 
             }
         }
 
         public DateTime end_date
         {
-            get { return _end_date; }
+            get { return this._end_date; }
             set
             {
                 // Check if DateTime
-                _end_date = value;
+                this._end_date = value;
             }
         }
 
         public void calculateGPA()
         {
             // Get all courses from database and calculate GPA then update it
+
+            Connection connect = new Connection();
+
+            connect.command.Connection = connect.connection;
+
+            connect.command.CommandText = "SELECT * from Courses WHERE 'Semester ID' = '" + this._id + "' ";
+            connect.reader = connect.command.ExecuteReader();
+
+            int index = 0;
+
+            while (connect.reader.Read())
+            {
+                // This returns boolean for the amount of values found, therefore if it is > 1 login is true
+
+                index++;
+
+                if (index > 0)
+                {
+                    // Login is true
+
+                    // Get info for student
+
+                    if ("" != connect.reader[5].ToString() || " " != connect.reader[5].ToString())
+                    {
+                        // Add to GPA
+
+
+
+
+                    }
+
+                   
+                }
+                else
+                {
+                    Console.WriteLine("No Courses Found");
+                    this._gpa = 0;
+                }
+            }
+
+            connect.closeConnection();
+
+  
         }
 
-        public string delete()
+        public bool delete()
         {
-            return "Issue Deleting This Semester";
+            Connection connect = new Connection();
+
+            connect.command.Connection = connect.connection;
+
+            connect.command.CommandText = "DELETE FROM Semesters WHERE 'Semester ID' = " + this._id + " ";
+            if (1 == connect.command.ExecuteNonQuery())
+            {
+                Console.WriteLine("Deleted Successful");
+                connect.closeConnection();
+                return true;
+            }
+
+            Console.WriteLine("Issue Deleting");
+            connect.closeConnection();
+            return false;
         }
 
-        public string save()
+        public bool save()
         {
             // Save semester in database
 
-            return "Issue Saving This Semester";
+            Connection connect = new Connection();
+
+            connect.command.Connection = connect.connection;
+
+            connect.command.CommandText = "INSERT INTO Semesters ('" + this._student_id + "', '" + this._start_date + "', '" + this._end_date + "', '" + this._gpa + "')";
+            if (1 == connect.command.ExecuteNonQuery())
+            {
+                Console.WriteLine("Deleted Successful");
+                connect.closeConnection();
+                return true;
+            }
+
+            Console.WriteLine("Issue Deleting");
+            connect.closeConnection();
+            return false;
         }
 
     }
